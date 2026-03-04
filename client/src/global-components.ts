6dae4082e4132102420adbe16a174761ae360905
Jerry Larsson <jerry.larsson@aditi.se>
2026-03-04 07:17:30 +0100

    dwad

--- client/src/global-components.ts
+++ client/src/global-components.ts
@@ -1,24 +1,57 @@
 export interface WebDeskGlobalComponentsRegistration {}
 
+type WdAuthGateType = typeof import('./components/framework/WdAuthGate.vue')['default']
+type WdDesktopType = typeof import('./components/framework/WdDesktop.vue')['default']
+type WdDialogType = typeof import('./components/framework/WdDialog.vue')['default']
+type WdLoginFormType = typeof import('./components/framework/WdLoginForm.vue')['default']
+type WdTaskbarType = typeof import('./components/framework/WdTaskbar.vue')['default']
+type WdTopMenuType = typeof import('./components/framework/WdTopMenu.vue')['default']
+type WdTopMenuDropdownType = typeof import('./components/framework/WdTopMenuDropdown.vue')['default']
+type WdTopMenuItemsType = typeof import('./components/framework/WdTopMenuItems.vue')['default']
+type WdWindowType = typeof import('./components/framework/WdWindow.vue')['default']
+
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+    WdAuthGate: WdAuthGateType
+    'wd-auth-gate': WdAuthGateType
+    WdDesktop: WdDesktopType
+    'wd-desktop': WdDesktopType
+    WdDialog: WdDialogType
+    'wd-dialog': WdDialogType
+    WdLoginForm: WdLoginFormType
+    'wd-login-form': WdLoginFormType
+    WdTaskbar: WdTaskbarType
+    'wd-taskbar': WdTaskbarType
+    WdTopMenu: WdTopMenuType
+    'wd-top-menu': WdTopMenuType
+    WdTopMenuDropdown: WdTopMenuDropdownType
+    'wd-top-menu-dropdown': WdTopMenuDropdownType
+    WdTopMenuItems: WdTopMenuItemsType
+    'wd-top-menu-items': WdTopMenuItemsType
+    WdWindow: WdWindowType
+    'wd-window': WdWindowType
+  }
+}
+
 declare module 'vue' {
   export interface GlobalComponents {
-    WdAuthGate: typeof import('./lib')['WdAuthGate']
-    'wd-auth-gate': typeof import('./lib')['WdAuthGate']
-    WdDesktop: typeof import('./lib')['WdDesktop']
-    'wd-desktop': typeof import('./lib')['WdDesktop']
-    WdDialog: typeof import('./lib')['WdDialog']
-    'wd-dialog': typeof import('./lib')['WdDialog']
-    WdLoginForm: typeof import('./lib')['WdLoginForm']
-    'wd-login-form': typeof import('./lib')['WdLoginForm']
-    WdTaskbar: typeof import('./lib')['WdTaskbar']
-    'wd-taskbar': typeof import('./lib')['WdTaskbar']
-    WdTopMenu: typeof import('./lib')['WdTopMenu']
-    'wd-top-menu': typeof import('./lib')['WdTopMenu']
-    WdTopMenuDropdown: typeof import('./lib')['WdTopMenuDropdown']
-    'wd-top-menu-dropdown': typeof import('./lib')['WdTopMenuDropdown']
-    WdTopMenuItems: typeof import('./lib')['WdTopMenuItems']
-    'wd-top-menu-items': typeof import('./lib')['WdTopMenuItems']
-    WdWindow: typeof import('./lib')['WdWindow']
-    'wd-window': typeof import('./lib')['WdWindow']
+    WdAuthGate: WdAuthGateType
+    'wd-auth-gate': WdAuthGateType
+    WdDesktop: WdDesktopType
+    'wd-desktop': WdDesktopType
+    WdDialog: WdDialogType
+    'wd-dialog': WdDialogType
+    WdLoginForm: WdLoginFormType
+    'wd-login-form': WdLoginFormType
+    WdTaskbar: WdTaskbarType
+    'wd-taskbar': WdTaskbarType
+    WdTopMenu: WdTopMenuType
+    'wd-top-menu': WdTopMenuType
+    WdTopMenuDropdown: WdTopMenuDropdownType
+    'wd-top-menu-dropdown': WdTopMenuDropdownType
+    WdTopMenuItems: WdTopMenuItemsType
+    'wd-top-menu-items': WdTopMenuItemsType
+    WdWindow: WdWindowType
+    'wd-window': WdWindowType
   }
 }
