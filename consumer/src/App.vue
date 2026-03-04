214946d7c1ce7b4cb09b48ae88dbb7b6907b84ce
Jerry Larsson <jerry.larsson@aditi.se>
2026-03-04 07:37:35 +0100

    asdasd

---
+++ consumer/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main>
+      <wd-desktop>
+        <wd-top-menu :main-menu-items="mainMenuItems"></wd-top-menu>
+      </wd-desktop>
+
+      <wd-taskbar></wd-taskbar>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+import type { WdTopMenuItem } from 'webdesk-ui'
+
+const mainMenuItems: WdTopMenuItem[] = []
+</script>
