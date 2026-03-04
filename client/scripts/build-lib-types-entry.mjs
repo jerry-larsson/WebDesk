6741e81d33c2491689f398c6117a998ce83445f5
Jerry Larsson <jerry.larsson@aditi.se>
2026-03-04 07:21:08 +0100

    awd

--- client/scripts/build-lib-types-entry.mjs
+++ client/scripts/build-lib-types-entry.mjs
@@ -3,7 +3,7 @@ import { resolve } from 'node:path'
 
 const outDir = resolve(process.cwd(), 'dist', 'library')
 const outFile = resolve(outDir, 'index.d.ts')
-const content = "export * from './lib'\nexport { default } from './lib'\n"
+const content = "import './global-components'\nexport * from './lib'\nexport { default } from './lib'\n"
 
 await mkdir(outDir, { recursive: true })
 await writeFile(outFile, content, 'utf8')
